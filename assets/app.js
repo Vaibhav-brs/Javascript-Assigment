715b633143c2b8fa14abc06ae089d9aeedd89fa3
Vaibhav-BRS <krishvaibu@gmail.com>
2024-05-23 10:08:01 +0530

    update

--- assets/app.js
+++ assets/app.js
@@ -4,59 +4,32 @@ class Car {
         this.color = color;
         this.manufacturer = manufacturer;
     }
-
-    getDetails() {
-        return `Color: ${this.color}, Manufacturer: ${this.manufacturer}`;
-    }
-
-    getOutputValues() {
-        return {
-            color: this.color,
-            manufacturer: this.manufacturer
-        };
-    }
 }
 
-// Define the RaceCar class that inherits from Car
+// Define the RaceCar class that inherits from the Car class
 class RaceCar extends Car {
-    constructor(numberOfSeats, manufacturer) {
-        super(null, manufacturer);  // Call the parent constructor with manufacturer only
+    constructor(manufacturer, numberOfSeats) {
+        super(null, manufacturer); // Call the parent constructor with color as null since it is not used here
         this.numberOfSeats = numberOfSeats;
     }
-
-    getDetails() {
-        return `Manufacturer: ${this.manufacturer}, Number of Seats: ${this.numberOfSeats}`;
-    }
-
-    getOutputValues() {
-        return {
-            manufacturer: this.manufacturer,
-            numberOfSeats: this.numberOfSeats
-        };
-    }
 }
 
-// Function to create a Car object
-function createCar() {
-    const color = document.getElementById('car-color').value;
-    const manufacturer = document.getElementById('car-manufacturer').value;
-    const car = new Car(color, manufacturer);
-
-    document.getElementById('car-details').innerText = car.getDetails();
-    const carOutput = car.getOutputValues();
-    document.getElementById('car-output').innerText = `Color: ${carOutput.color}, Manufacturer: ${carOutput.manufacturer}`;
+// Function to display the race car details
+function displayRaceCarDetails() {
+    const manufacturer = document.getElementById('manufacturer').value;
+    const numberOfSeats = document.getElementById('numberOfSeats').value;
+    
+    // Create an instance of the RaceCar class
+    const raceCar = new RaceCar(manufacturer, numberOfSeats);
+    
+    // Display the race car details on the webpage
+    const raceCarDetailsDiv = document.getElementById('racecar-details');
+    raceCarDetailsDiv.innerHTML = `<h2>RaceCar Details</h2>
+                                   <p>Manufacturer: ${raceCar.manufacturer}</p>
+                                   <p>Number of Seats: ${raceCar.numberOfSeats}</p>`;
 }
 
-// Function to create a RaceCar object
-function createRaceCar() {
-    const numberOfSeats = document.getElementById('racecar-seats').value;
-    const manufacturer = document.getElementById('racecar-manufacturer').value;
-    const raceCar = new RaceCar(numberOfSeats, manufacturer);
 
-    document.getElementById('racecar-details').innerText = raceCar.getDetails();
-    const raceCarOutput = raceCar.getOutputValues();
-    document.getElementById('racecar-output').innerText = `Number of Seats: ${raceCarOutput.numberOfSeats}, Manufacturer: ${raceCarOutput.manufacturer}`;
-}
 
 
 
